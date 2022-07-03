7627ecb34bd2590dcf1001776ba70e16a79635e3
keisa <kesksjxhx@gmail.com>
2022-07-03 23:39:36 +0900

    //

--- spring2/src/main/java/com/example/spring2/jwt/JwtTokenProvider.java
+++ spring2/src/main/java/com/example/spring2/jwt/JwtTokenProvider.java
@@ -1,11 +1,23 @@
 package com.example.spring2.jwt;
 
+import com.example.spring2.exception.ExpiredAccessTokenException;
+import com.example.spring2.exception.ExpiredRefreshTokenException;
+import com.example.spring2.exception.IncorrectTokenException;
+import com.example.spring2.exception.InvalidTokenException;
+import com.example.spring2.service.CustomUserDetailsService;
+import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
@@ -25,12 +37,83 @@ public class JwtTokenProvider {
     @Value("${jwt.exp.refresh}")
     private Long REFRESH_TOKEN_EXPIRATION_TIME;
 
-    private
+    private CustomUserDetailsService customUserDetailsService;
 
     @Value("${jwt.secret}")
     public void setSecretKey(String secretKey) {
         this.SECRET_KEY = Base64.getEncoder().encodeToString(secretKey.getBytes(StandardCharsets.UTF_8));
     }
 
+    public String createAccessToken(String username) {
+        return Jwts.builder()
+                .setHeaderParam("typ", "jwt")
+                .setSubject(username)
+                .claim("typ", "access")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
 
+    public String createRefreshToken(String username) {
+        return Jwts.builder()
+                .setHeaderParam("typ", "jwt")
+                .setSubject(username)
+                .claim("typ", "refresh")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(getUsername(token).getSubject());
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String token = request.getHeader(HEADER);
+
+        if (StringUtils.hasText(token) && token.startsWith(PREFIX)) {
+            return token.substring(7);
+        }
+        return null;
+    }
+
+    public Claims getUsername(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (MalformedJwtException | UnsupportedJwtException e) {
+            throw IncorrectTokenException.EXCEPTION;
+        } catch (ExpiredJwtException e) {
+            throw ExpiredAccessTokenException.EXCEPTION;
+        } catch (Exception e) {
+            throw InvalidTokenException.EXCEPTION;
+        }
+    }
+
+    public boolean validateToken(String token) {
+        return !getUsername(token)
+                .getExpiration()
+                .before(new Date());
+    }
+
+    public boolean isRefreshToken(String refreshToken) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .parseClaimsJws(refreshToken)
+                    .getBody();
+            return claims.get("type").equals("refresh") && !claims.getExpiration().before(new Date());
+        } catch (MalformedJwtException | UnsupportedJwtException e) {
+            throw IncorrectTokenException.EXCEPTION;
+        } catch (ExpiredJwtException e) {
+            throw ExpiredRefreshTokenException.EXCEPTION;
+        } catch (Exception e) {
+            throw InvalidTokenException.EXCEPTION;
+        }
+    }
 }
